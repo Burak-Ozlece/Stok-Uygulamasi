eb3fde106974f15d7ee13b8714f56fe18282c7ef
Yusuf Altuntaş <100843870+yusuf-altnts@users.noreply.github.com>
2024-11-20 21:11:40 +0300

    Login Tasarım Tamamdır

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,66 +28,102 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             button1 = new Button();
-            label1 = new Label();
-            label2 = new Label();
             textBox1 = new TextBox();
             textBox2 = new TextBox();
+            pictureBox2 = new PictureBox();
+            label3 = new Label();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(166, 276);
+            button1.BackColor = Color.White;
+            button1.FlatStyle = FlatStyle.Popup;
+            button1.Font = new Font("Times New Roman", 18F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.Black;
+            button1.Location = new Point(131, 281);
             button1.Name = "button1";
-            button1.Size = new Size(94, 29);
+            button1.Size = new Size(189, 48);
             button1.TabIndex = 0;
-            button1.Text = "Giriş";
-            button1.UseVisualStyleBackColor = true;
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Location = new Point(154, 143);
-            label1.Name = "label1";
-            label1.Size = new Size(92, 20);
-            label1.TabIndex = 1;
-            label1.Text = "Kullanıcı Adı";
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Location = new Point(154, 206);
-            label2.Name = "label2";
-            label2.Size = new Size(50, 20);
-            label2.TabIndex = 2;
-            label2.Text = "Parola";
+            button1.Text = "Giris";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            button1.MouseLeave += button1_MouseLeave_1;
+            button1.MouseHover += button1_MouseHover;
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(154, 166);
+            textBox1.BackColor = Color.White;
+            textBox1.Font = new Font("Franklin Gothic Medium", 12F, FontStyle.Italic);
+            textBox1.Location = new Point(51, 182);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(125, 27);
+            textBox1.PlaceholderText = "Kullanıcı Adı";
+            textBox1.Size = new Size(318, 30);
             textBox1.TabIndex = 3;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(154, 229);
+            textBox2.Font = new Font("Franklin Gothic Medium", 12F, FontStyle.Italic);
+            textBox2.Location = new Point(51, 228);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(125, 27);
+            textBox2.PlaceholderText = "Şifre";
+            textBox2.Size = new Size(318, 30);
             textBox2.TabIndex = 4;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(424, 0);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(704, 628);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 6;
+            pictureBox2.TabStop = false;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Stencil", 36F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label3.Location = new Point(114, 68);
+            label3.Name = "label3";
+            label3.Size = new Size(206, 71);
+            label3.TabIndex = 8;
+            label3.Text = "LOGIN";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.White;
+            label1.Font = new Font("Stencil", 22.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Black;
+            label1.Location = new Point(0, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(42, 44);
+            label1.TabIndex = 9;
+            label1.Text = "X";
+            label1.Click += label1_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            BackColor = Color.White;
+            ClientSize = new Size(1127, 626);
+            Controls.Add(label1);
+            Controls.Add(label3);
+            Controls.Add(pictureBox2);
             Controls.Add(textBox2);
             Controls.Add(textBox1);
-            Controls.Add(label2);
-            Controls.Add(label1);
             Controls.Add(button1);
+            FormBorderStyle = FormBorderStyle.None;
             Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -95,9 +131,10 @@
         #endregion
 
         private Button button1;
-        private Label label1;
-        private Label label2;
         private TextBox textBox1;
         private TextBox textBox2;
+        private PictureBox pictureBox2;
+        private Label label3;
+        private Label label1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,38 @@ namespace Stok_Uygulaması
         {
             InitializeComponent();
         }
+
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+
+
+        private void button1_MouseHover(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.Black;
+            button1.ForeColor = Color.White;
+        }
+
+
+
+        private void button1_MouseLeave_1(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.White;
+            button1.ForeColor = Color.Black;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
